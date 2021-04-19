b5a160cd4fc7e455f79bae71016ff985e0880a69
Stephen Hubbard <hubbardgames@gmail.com>
2021-04-19 08:16:08 -0600

    Sword attacks

---
+++ Assets/Scripts/Player/EquippedItemFaceMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedItemFaceMouse : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        FaceMouse();
+    }
+
+    private void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(transform.position.x - mousePosition.x, transform.position.y - mousePosition.y);
+
+        transform.right = -direction;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0) && !isAttacking)
+        {
+            StartCoroutine(AttackCo());
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetTrigger("attack");
+        isAttacking = true;
+        yield return new WaitForSeconds(.3f);
+        isAttacking = false;
+    }
+
+    public bool GetIsAttackingBool()
+    {
+        return isAttacking;
+    }
+
+}
